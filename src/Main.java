e4534da9d8c925ee52545a7ab5bf15d2abaf7d84
TpyMuxa <tpymuxa1133@gmail.com>
2023-09-16 06:01:04 +0500

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     // Объявить метод «сгенерироватьМассив»
@@ -22,8 +23,27 @@ public class Main {
         int paymentsPerMonth = 0;
         for (int i = 0; i < arr.length; i++) {
             paymentsPerMonth = paymentsPerMonth + arr[i];
-            System.out.println(arr[i]);
         }
         System.out.println("Сумма трат за месяц составила " + paymentsPerMonth + " рублей");
+    }
+
+    public static void task2() {
+        int[] arr = generateRandomArray();
+        // int[] arr — объявление массива
+        // generateRandomArray() — вызов метода «сгенерироватьМассив»
+        // Далее пишем код для задачи
+        System.out.println("Задание 2");
+        int minCostDay = arr[0];
+        int maxCostDay = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (maxCostDay < arr[i]) {
+                maxCostDay = arr[i];
+            }
+            if (minCostDay > arr[i]) {
+                minCostDay = arr[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + minCostDay + " рублей");
+        System.out.println("Максимальная сумма трат за день составила " + maxCostDay + " рублей");
     }
 }
